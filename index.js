ed6b53826d7ab44cb0655c71a1ea1d054859ebcd
Marais Devis <maraisdcdevis@gmail.com>
2024-05-10 19:22:57 +0200

    Modify a few files and commit as backup

---
+++ index.js
@@ -0,0 +1,30 @@
+import fs from 'node:fs';
+import * as cheerio from 'cheerio';
+import fetch from 'node-fetch';
+
+// make "memes" directory
+const directoryPath = './memes';
+
+// Check if the directory exists
+if (!fs.existsSync(directoryPath)) {
+  // If it doesn't exist, create the directory
+  fs.mkdirSync(directoryPath);
+
+  console.log(`Directory '${directoryPath}' created.`);
+} else {
+  console.log(`Directory '${directoryPath}' already exists.`);
+}
+
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const body = await response.text();
+
+console.log(body);
+
+const $ = cheerio.load(body);
+
+$.html();
+
+console.log($);
+#images > div:nth-child(1) > a > img
